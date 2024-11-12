7c44c072c6b60e108a9ce8de7cc6a2c45e6cc4e9
shogo.takasaki <stakasaki707@gmail.com>
2024-11-12 22:45:27 +0900

    Add run method

--- app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
@@ -15,9 +15,12 @@
  */
 package com.example.racetracker.ui
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
+import kotlin.math.max
 
 /**
  * This class represents a state holder for race participant.
@@ -39,6 +42,18 @@ class RaceParticipant(
      */
     var currentProgress by mutableStateOf(initialProgress)
         private set
+
+    suspend fun run() {
+        try {
+            while (currentProgress < maxProgress) {
+                delay(progressDelayMillis)
+                currentProgress += progressIncrement
+            }
+        } catch (e: Exception) {
+            Log.e("RaceParticipant", "$name: ${e.message}")
+            throw e
+        }
+    }
 
     /**
      * Regardless of the value of [initialProgress] the reset function will reset the
